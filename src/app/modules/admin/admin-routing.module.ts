151d8b060980c41131910ff3c74863d076730daf
Rafael Escalera <escalerarodriguezr@gmail.com>
2022-02-17 12:31:48 +0100

    load-admin-module

--- src/app/modules/admin/admin-routing.module.ts
+++ src/app/modules/admin/admin-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "../auth/login/login.component";
+import {AdminComponent} from "./admin.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+      {
+        path: '',
+        component: AdminComponent
+      },
+      {
+        path: '**',
+        redirectTo: ''
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
