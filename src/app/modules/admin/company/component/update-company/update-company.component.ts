11707ae648c92efd69dfc2f9403e9f5ced6233dc
Rafael Escalera <escalerarodriguezr@gmail.com>
2022-05-24 15:03:23 +0200

    feature/13-get-company-service/complete

--- src/app/modules/admin/company/component/update-company/update-company.component.ts
+++ src/app/modules/admin/company/component/update-company/update-company.component.ts
@@ -4,6 +4,8 @@ import {HttpErrorResponse} from "@angular/common/http";
 import {GetCompanyInterface} from "../../service/get-company/get-company-interface";
 import {HttpBaseService} from "../../../shared/service/http-base/http-base.service";
 import {Company} from "../../../../../model/company/company.model";
+import {FormBuilder, FormControlStatus, FormGroup, Validators} from "@angular/forms";
+import Swal from "sweetalert2";
 
 @Component({
   selector: 'app-update-company',
@@ -12,14 +14,22 @@ import {Company} from "../../../../../model/company/company.model";
 })
 export class UpdateCompanyComponent implements OnInit {
 
-
   private company:Company|undefined
+  public updateCompanyForm:FormGroup
+
+  public nameHasError:boolean = false;
 
   constructor(
     private getCompanyService:GetCompanyService,
-    private httpBaseService:HttpBaseService
-  ) {
-    console.log("carga");
+    private httpBaseService:HttpBaseService,
+    private fb:FormBuilder
+  )
+  {
+    this.updateCompanyForm = this.fb.group({
+      name:['',Validators.required],
+      legalDocument:[''],
+      address:['']
+    });
   }
 
   ngOnInit(): void {
@@ -27,7 +37,6 @@ export class UpdateCompanyComponent implements OnInit {
     this.httpBaseService.screenLock()
     this.getCompanyService.invoke().subscribe({
       next: (response:GetCompanyInterface)=>{
-
 
         this.company = new Company(
           response.id,
@@ -37,14 +46,45 @@ export class UpdateCompanyComponent implements OnInit {
           response.address
         );
 
-        console.log(this.company);
+        this.setCompanyInitValues();
         this.httpBaseService.screenUnLock();
       },
       error: (error:HttpErrorResponse)=>{
-        console.log(error);
         this.httpBaseService.screenUnLock();
+        if( error.status == 404 ) {
+          Swal.fire('Error', 'Error: '+error.error.message, 'error' );
+        }else{
+          Swal.fire('Error', 'Se ha producido un error inesperado', 'error' );
+        }
       }
+    });
+
+    this.updateCompanyForm.get('name')?.statusChanges.subscribe({
+      next:(status:FormControlStatus)=>{
+      this.nameHasError = status == 'INVALID';
+    }
     })
+  }
+
+  private setCompanyInitValues():void
+  {
+    const initCompany = {
+      name:this.company?.name,
+      legalDocument:this.company?.legalDocument,
+      address:this.company?.legalDocument
+    }
+    this.updateCompanyForm.setValue(initCompany)
+  }
+
+  get formValid():boolean
+  {
+    return this.updateCompanyForm.valid;
+  }
+
+
+  public submit():void
+  {
+    console.log(this.updateCompanyForm.value);
   }
 
 }
