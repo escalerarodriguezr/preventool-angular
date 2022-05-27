5fd982466257650dee06105e9a0e724f6d18c1a8
Rafael Escalera <escalerarodriguezr@gmail.com>
2022-05-27 14:30:42 +0200

    o-multiple/feature/2-create-organization-service/in-progress/1

--- src/app/modules/admin/organization/component/create-organization/create-organization.component.ts
+++ src/app/modules/admin/organization/component/create-organization/create-organization.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControlStatus, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-create-organization',
@@ -7,9 +8,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateOrganizationComponent implements OnInit {
 
-  constructor() { }
+  public createOrganizationForm: FormGroup;
+
+  public nameHasError:boolean = false;
+  public emailRequiredError:boolean = false;
+  public emailEmailError:boolean = false;
+
+  constructor(
+    private fb:FormBuilder
+  ) {
+
+    this.createOrganizationForm = this.fb.group({
+      name:[null,Validators.required],
+      email:[null,[Validators.required,Validators.email]],
+      legalDocument:[null],
+      address:[null]
+    })
+  }
 
   ngOnInit(): void {
+
+    this.createOrganizationForm.get('name')?.statusChanges.subscribe({
+      next: (status:FormControlStatus)=>{
+        this.nameHasError = status == 'INVALID';
+      }
+    });
+
+    this.createOrganizationForm.get('email')?.statusChanges.subscribe({
+      next:(status:FormControlStatus)=>{
+        this.emailRequiredError =  !!this.createOrganizationForm.get('email')?.hasError('required');
+        this.emailEmailError = !!this.createOrganizationForm.get('email')?.hasError('email');
+      }
+    });
+    
+  }
+
+  get formValid():boolean
+  {
+    return this.createOrganizationForm.valid;
+  }
+
+  get formDirty():boolean
+  {
+    return this.createOrganizationForm.dirty
+  }
+
+  get emailInvalid():boolean
+  {
+    return this.emailRequiredError || this.emailEmailError;
+  }
+
+  get formCanSubmit():boolean
+  {
+    return (this.formValid && this.formDirty);
+  }
+
+  public submit():void
+  {
+    console.log(this.createOrganizationForm.value);
   }
 
 }
